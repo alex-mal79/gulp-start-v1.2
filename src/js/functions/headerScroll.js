5be34d801a45e01c5e6d6e9b91bccc14d6199ea2
unknown <D:\The Bat!>
2022-05-26 18:18:29 +0300

    Добавлены функция меню js бургер

--- src/js/functions/headerScroll.js
+++ src/js/functions/headerScroll.js
@@ -2,13 +2,14 @@
 // Шапка при скролле
 //================================================================
 export const headerScroll = () => {
-    const header = document.querySelector('.header');
+    const header = document.querySelector('[data-scroll]');
+
     if(header){
         let lastScroll = 0;
-        const defaultOffset = 150;
+        const defaultOffset = header.dataset.scroll;
         const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
         const containHide = () => header.classList.contains('scroll--active');
-        window.addEventListener('scroll--active', () => {
+        window.addEventListener('scroll', () => {
             if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
                 //scroll down
                 header.classList.add('scroll--active');
